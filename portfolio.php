2ae7f77a657751b083600da47908e6fea63a7a86
VikIsCool123 <viktorhorvath876@gmail.com>
2026-04-05 18:42:55 +0200

    make portfolio dynamic

--- portfolio.php
+++ portfolio.php
@@ -18,35 +18,14 @@
                     <h1>Portfólio</h1>
                 </div>
             </section>
-              <section class="container">
-                <div class="row">
-                  <div class="col-25 portfolio text-white text-center" id="portfolio-1">
-                      Web stránka 1
-                  </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-2">
-                        Web stránka 2
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-3">
-                        Web stránka 3
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-4">
-                        Web stránka 4
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-5">
-                        Web stránka 5
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-6">
-                        Web stránka 6
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-7">
-                        Web stránka 7
-                    </div>
-                    <div class="col-25 portfolio text-white text-center" id="portfolio-8">
-                        Web stránka 8
-                    </div>
-                </div>
+              <section class="container container-portfolio">
+                <?php
+                for ($i=1;$i<9;$i++){
+                  echo "<div class='col-25 portfolio text-white text-center' id='portfolio-$i'>";
+                      echo "Web stránka " . $i;
+                  echo "</div>";
+                }
+                ?>
             </section>   
 
         </main>
